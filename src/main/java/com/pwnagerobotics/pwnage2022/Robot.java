44dfae38f054fb6ed4e76026556015c0fa29a2a2
billy13O1 <aidankeighron@gmail.com>
2022-03-29 17:43:06 -0500

    moved code to drive

--- src/main/java/com/pwnagerobotics/pwnage2022/Robot.java
+++ src/main/java/com/pwnagerobotics/pwnage2022/Robot.java
@@ -5,16 +5,11 @@
 package com.pwnagerobotics.pwnage2022;
 
 import com.pwnagerobotics.pwnage2022.humans.driver.XboxDriver;
-import com.team254.lib.util.LatchedBoolean;
+import com.pwnagerobotics.pwnage2022.subsystems.Drive;
+import com.team254.lib.subsystems.SubsystemManager;
 
-import edu.wpi.first.wpilibj.AnalogEncoder;
-import edu.wpi.first.wpilibj.PneumaticHub;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.Timer;
 
 
 /**
@@ -28,45 +23,9 @@ import edu.wpi.first.wpilibj.Solenoid;
  */
 public class Robot extends TimedRobot {
 
-  public static class RotationEncoderOffset {
-    public static final double mEncoderFrontLeftOffset = 0.601;
-    public static final double mEncoderBackLeftOffset = 0.185;
-    public static final double mEncoderFrontRightOffset = 0.590;
-    public static final double mEncoderBackRightOffset = 0.248;
-  }
-
-  // Right
-  public MotorController mMotorFrontRightRotation = new PWMMotorController("motor0", 0) {};
-  public MotorController mMotorFrontRightDrive = new PWMMotorController("motor1", 1) {};
-  public MotorController mMotorBackRightRotation = new PWMMotorController("motor2", 2) {};
-  public MotorController mMotorBackRightDrive = new PWMMotorController("motor3", 3) {};
-
-  // Left
-  public MotorController mMotorFrontLeftRotation = new PWMMotorController("motor4", 4) {};
-  public MotorController mMotorFrontLeftDrive = new PWMMotorController("motor5", 5) {};
-  public MotorController mMotorBackLeftRotation = new PWMMotorController("motor6", 6) {};
-  public MotorController mMotorBackLeftDrive = new PWMMotorController("motor9", 9) {};
-
-  // Rotation Encoders
-  private AnalogEncoder mEncoderFrontLeftRotation = new AnalogEncoder(0);
-  private AnalogEncoder mEncoderBackLeftRotation = new AnalogEncoder(1);
-  private AnalogEncoder mEncoderFrontRightRotation = new AnalogEncoder(2);
-  private AnalogEncoder mEncoderBackRightRotation = new AnalogEncoder(3);
-
-  // Pnumatics
-  public Solenoid driveShift = new Solenoid(PneumaticsModuleType.REVPH, 0);
-  private LatchedBoolean mToggleShift = new LatchedBoolean();
-  private boolean mSetShift = false;
-
-  // Drive
-  public double mSpeed = 0.0;
-  public double mRotation = 0.0;
-  public double deadband = 0.1;
-  public double error = 0.08;
-  public double slowDown = 1;
-  public double turnSpeed = 0.8;
-  public static final double kMaxValue = 0.974;
-
+  // Subsystemss
+  private final SubsystemManager mSubsystemManager = SubsystemManager.getInstance();
+  private final Drive mDrive = Drive.getInstance();
 
   public Robot(){
     super(0.04);
@@ -76,165 +35,63 @@ public class Robot extends TimedRobot {
   private XboxDriver mDriver = new XboxDriver();
 
   @Override
-  public void teleopInit() {
-
-  }
-
-  @Override
-  public void teleopPeriodic() {
-
-    if (Math.abs(mDriver.getPositionX()) > deadband || Math.abs(mDriver.getPositionY()) > deadband) {
-      double angle = Math.toDegrees(Math.atan2(mDriver.getPositionX(), mDriver.getPositionY()));
-      double speed = Math.sqrt(Math.pow(Math.abs(mDriver.getPositionX()), 2) + Math.pow(Math.abs(mDriver.getPositionY()), 2));
-      angle = (angle > 0) ? angle : angle + 360;
-      speed = (speed > 1) ? 1 : speed;
-      angle /= 360;
-      setDrive(speed, angle);
-    }
-    else if (mDriver.getDPad() == 0) {
-      setDrive(0, 0);
-    }
-    else {
-      setDrive(0, -1);
-    }
-
-    if (Math.abs(mDriver.getRotationX()) > deadband) {
-      rotate(-mDriver.getRotationX());
-    }
-    else {
-      runDrive();
-    }
-  }
-
-  public void runDrive() {
-    mSpeed *= slowDown;
-    mMotorFrontRightDrive.set(-mSpeed);
-    mMotorBackRightDrive.set(-mSpeed);
-    mMotorFrontLeftDrive.set(-mSpeed);
-    mMotorBackLeftDrive.set(mSpeed);
-
-    if (Math.abs(mEncoderFrontRightRotation.getAbsolutePosition() - getMotorRotation(mRotation, RotationEncoderOffset.mEncoderFrontRightOffset, mEncoderFrontRightRotation.getAbsolutePosition())) > error) {
-      mMotorFrontRightRotation.set(turnSpeed * getSign(mEncoderFrontRightRotation.getAbsolutePosition(), getMotorRotation(mRotation, RotationEncoderOffset.mEncoderFrontRightOffset, mEncoderFrontRightRotation.getAbsolutePosition()), 0));
-    }
-    else {
-      mMotorFrontRightRotation.set(0);
-    }
-    if (Math.abs(mEncoderBackRightRotation.getAbsolutePosition() - getMotorRotation(mRotation, RotationEncoderOffset.mEncoderBackRightOffset, mEncoderBackRightRotation.getAbsolutePosition())) > error) {
-      mMotorBackRightRotation.set(turnSpeed * getSign(mEncoderBackRightRotation.getAbsolutePosition(), getMotorRotation(mRotation, RotationEncoderOffset.mEncoderBackRightOffset, mEncoderBackRightRotation.getAbsolutePosition()), 1));
-    }
-    else {
-      mMotorBackRightRotation.set(0);
-    }
-    if (Math.abs(mEncoderFrontLeftRotation.getAbsolutePosition() - getMotorRotation(mRotation, RotationEncoderOffset.mEncoderFrontLeftOffset, mEncoderFrontLeftRotation.getAbsolutePosition())) > error) {
-      mMotorFrontLeftRotation.set(turnSpeed * getSign(mEncoderBackRightRotation.getAbsolutePosition(), getMotorRotation(mRotation, RotationEncoderOffset.mEncoderBackRightOffset, mEncoderFrontLeftRotation.getAbsolutePosition()), 2));
-    }
-    else {
-      mMotorFrontLeftRotation.set(0);
-    }
-    if (Math.abs(mEncoderBackLeftRotation.getAbsolutePosition() - getMotorRotation(mRotation, RotationEncoderOffset.mEncoderBackLeftOffset, mEncoderBackLeftRotation.getAbsolutePosition())) > error) {
-      mMotorBackLeftRotation.set(turnSpeed * getSign(mEncoderBackRightRotation.getAbsolutePosition(), getMotorRotation(mRotation, RotationEncoderOffset.mEncoderBackRightOffset, mEncoderBackLeftRotation.getAbsolutePosition()), 3));
-    }
-    else {
-      mMotorBackLeftRotation.set(0);
-    }
-  }
-
-  public double getDistance(double encoder, double controller, int id) {
-    if (controller > encoder) {
-      return (encoder - controller) * ((controller - encoder > 0.5) ? -1 : 1);
-    }
-    if (encoder > controller) {
-      return (encoder - controller) * ((encoder - controller > 0.5) ? -1 : 1);
-    }
-    else {
-      return 0;
-    }
-  }
-
-  public double getSign(double encoder, double controller, int id) {
-    return Math.signum(getDistance(encoder, controller, id));
-  }
-
-  public void rotate(double speed) {
-    speed *= -slowDown;
-    mMotorFrontRightDrive.set(-speed);
-    mMotorBackRightDrive.set(-speed);
-    mMotorFrontLeftDrive.set(-speed);
-    mMotorBackLeftDrive.set(speed);
-    mRotation = 0.25/2 + 0.25;
-    if (Math.abs(mEncoderFrontRightRotation.getAbsolutePosition() - getMotorRotation(mRotation, RotationEncoderOffset.mEncoderFrontRightOffset, mEncoderFrontRightRotation.getAbsolutePosition())) > error) {
-      mMotorFrontRightRotation.set(turnSpeed * getSign(mEncoderFrontRightRotation.getAbsolutePosition(), getMotorRotation(mRotation, RotationEncoderOffset.mEncoderFrontRightOffset, mEncoderFrontRightRotation.getAbsolutePosition()), 0));
-    }
-    else {
-      mMotorBackRightRotation.set(0);
-    }
-    mRotation = 0.25/2 + 0.50;
-    if (Math.abs(mEncoderBackRightRotation.getAbsolutePosition() - getMotorRotation(mRotation, RotationEncoderOffset.mEncoderBackRightOffset, mEncoderBackRightRotation.getAbsolutePosition())) > error) {
-      mMotorBackRightRotation.set(turnSpeed * getSign(mEncoderBackRightRotation.getAbsolutePosition(), getMotorRotation(mRotation, RotationEncoderOffset.mEncoderBackRightOffset, mEncoderBackRightRotation.getAbsolutePosition()), 1));
-    }
-    else {
-      mMotorBackRightRotation.set(0);
-    }
-    mRotation = 0.25/2;
-    if (Math.abs(mEncoderFrontLeftRotation.getAbsolutePosition() - getMotorRotation(mRotation, RotationEncoderOffset.mEncoderFrontLeftOffset, mEncoderFrontLeftRotation.getAbsolutePosition())) > error) {
-      mMotorFrontLeftRotation.set(turnSpeed * getSign(mEncoderBackRightRotation.getAbsolutePosition(), getMotorRotation(mRotation, RotationEncoderOffset.mEncoderBackRightOffset, mEncoderFrontLeftRotation.getAbsolutePosition()), 1));
-    }
-    else {
-      mMotorFrontLeftRotation.set(0);
-    }
-    mRotation = 0.25/2 + 0.75;
-    if (Math.abs(mEncoderBackLeftRotation.getAbsolutePosition() - getMotorRotation(mRotation, RotationEncoderOffset.mEncoderBackLeftOffset, mEncoderBackLeftRotation.getAbsolutePosition())) > error) {
-      mMotorBackLeftRotation.set(turnSpeed * getSign(mEncoderBackRightRotation.getAbsolutePosition(), getMotorRotation(mRotation, RotationEncoderOffset.mEncoderBackRightOffset, mEncoderBackLeftRotation.getAbsolutePosition()), 1));
-    }
-    else {
-      mMotorBackLeftRotation.set(0);
-    }
-  }
-
-  private void setDrive(double speed, double rotation) {
-    mSpeed = speed;
-    if (rotation != -1)
-      mRotation = rotation;
-  }
-
-  private double getMotorRotation(double value, double zero, double encoder) {
-    value += zero;
-    if (value > kMaxValue) {
-      value -= kMaxValue;
-    }
-    return value;
-  }
-
-  @Override
   public void robotInit() {
-
+    mSubsystemManager.setSubsystems(
+      mDrive
+    );
   }
 
   @Override
   public void robotPeriodic() {
-    SmartDashboard.putNumber("Front Left", mEncoderFrontLeftRotation.getAbsolutePosition());
-    SmartDashboard.putNumber("Back Left", mEncoderBackLeftRotation.getAbsolutePosition());
-    SmartDashboard.putNumber("Front Right", mEncoderFrontRightRotation.getAbsolutePosition());
-    SmartDashboard.putNumber("Back Right", mEncoderBackRightRotation.getAbsolutePosition());
-    SmartDashboard.putBoolean("Shifter", mSetShift);
+    mSubsystemManager.outputToSmartDashboard();
   }
 
   @Override
   public void disabledInit() {
-
+    var timestamp = Timer.getFPGATimestamp();
+    mSubsystemManager.executeEnabledLoopStops(timestamp);
   }
 
   @Override
   public void disabledPeriodic() {
-    
+    var timestamp = Timer.getFPGATimestamp();
+    mSubsystemManager.executeDisabledLoops(timestamp);
   }
 
   @Override
-  public void autonomousInit() { }
+  public void autonomousInit() { 
+    var timestamp = Timer.getFPGATimestamp();
+    mSubsystemManager.executeEnabledLoopStarts(timestamp);
+  }
 
   @Override
-  public void autonomousPeriodic() { }
+  public void autonomousPeriodic() {
+    mSubsystemManager.executeEnabledLoops(Timer.getFPGATimestamp());
+  }
 
+  @Override
+  public void teleopInit() {
+    mSubsystemManager.executeEnabledLoopStops(Timer.getFPGATimestamp());
+    mSubsystemManager.executeEnabledLoopStarts(Timer.getFPGATimestamp());
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    var timestamp = Timer.getFPGATimestamp();
+
+    double rotation = mDriver.getRotationX();
+    double throttle = mDriver.getPositionY();
+    double strafe = mDriver.getPositionX();
+    boolean wantZero = mDriver.getDPad() == 0;
+
+    mDrive.setSwerveDrive(throttle, strafe, rotation);
+
+    if (wantZero) {
+      mDrive.zeroDrive();
+    }
+
+    mSubsystemManager.executeEnabledLoops(timestamp);
+  }
 
   @Override
   public void testInit() { }
--- src/main/java/com/pwnagerobotics/pwnage2022/subsystems/Drive.java
+++ src/main/java/com/pwnagerobotics/pwnage2022/subsystems/Drive.java
@@ -1,5 +1,219 @@
 package com.pwnagerobotics.pwnage2022.subsystems;
 
-public class Drive {
+import com.pwnagerobotics.pwnage2022.subsystems.Drive;
+
+import edu.wpi.first.wpilibj.AnalogEncoder;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.team254.lib.subsystems.Subsystem;
+
+public class Drive extends Subsystem {
+    // Singleton Drive
+    public static Drive mInstance;
+    public synchronized static Drive getInstance() {
+        if (mInstance == null) mInstance = new Drive();
+        return mInstance;
+    }
+
+    // Zeros
+    public static class RotationEncoderOffset {
+        public static final double kEncoderFrontLeftOffset = 0.601;
+        public static final double kEncoderBackLeftOffset = 0.185;
+        public static final double kEncoderFrontRightOffset = 0.590;
+        public static final double kEncoderBackRightOffset = 0.248;
+    }
+
+    // Right
+    private final MotorController kMotorFrontRightRotation = new PWMMotorController("motor0", 0) {};
+    private final MotorController kMotorFrontRightDrive = new PWMMotorController("motor1", 1) {};
+    private final MotorController kMotorBackRightRotation = new PWMMotorController("motor2", 2) {};
+    private final MotorController kMotorBackRightDrive = new PWMMotorController("motor3", 3) {};
+
+    // Left
+    private final MotorController kMotorFrontLeftRotation = new PWMMotorController("motor4", 4) {};
+    private final MotorController kMotorFrontLeftDrive = new PWMMotorController("motor5", 5) {};
+    private final MotorController kMotorBackLeftRotation = new PWMMotorController("motor6", 6) {};
+    private final MotorController kMotorBackLeftDrive = new PWMMotorController("motor9", 9) {};
+
+    // Rotation Encoders
+    private static final AnalogEncoder kEncoderFrontLeftRotation = new AnalogEncoder(0);
+    private static final AnalogEncoder kEncoderBackLeftRotation = new AnalogEncoder(1);
+    private static final AnalogEncoder kEncoderFrontRightRotation = new AnalogEncoder(2);
+    private static final AnalogEncoder kEncoderBackRightRotation = new AnalogEncoder(3);
+
+    // Drive
+    private static final double kMaxValue = 0.974;
+    private double mSpeed = 0.0;
+    private double mRotation = 0.0;
+    private double mDeadband = 0.1;
+    private double mError = 0.08;
+    private double mSlowDown = 1;
+    private double mTurnSpeed = 0.8;
+
+    public synchronized void setSwerveDrive(double throttle, double strafe, double rotation) {
+        if (Math.abs(strafe) > mDeadband || Math.abs(throttle) > mDeadband) {
+            double angle = Math.toDegrees(Math.atan2(strafe, throttle));
+            double speed = Math.sqrt(Math.pow(Math.abs(strafe), 2) + Math.pow(Math.abs(throttle), 2));
+            angle = (angle > 0) ? angle : angle + 360;
+            speed = (speed > 1) ? 1 : speed;
+            angle /= 360;
+            setDrive(speed, angle);
+          }
+          else {
+            setDrive(0, -1);
+          }
+      
+          if (Math.abs(rotation) > mDeadband) {
+            rotate(-rotation);
+          }
+          else {
+            runDrive();
+          }
+    }
+
+    public synchronized void zeroDrive() {
+        setDrive(0, 0);
+    }
+
+    public void runDrive() {
+        mSpeed *= mSlowDown;
+        kMotorFrontRightDrive.set(-mSpeed);
+        kMotorBackRightDrive.set(-mSpeed);
+        kMotorFrontLeftDrive.set(-mSpeed);
+        kMotorBackLeftDrive.set(mSpeed);
     
+        if (Math.abs(kEncoderFrontRightRotation.getAbsolutePosition() - getMotorRotation(mRotation, RotationEncoderOffset.kEncoderFrontRightOffset, kEncoderFrontRightRotation.getAbsolutePosition())) > mError) {
+          kMotorFrontRightRotation.set(mTurnSpeed * getSign(kEncoderFrontRightRotation.getAbsolutePosition(), getMotorRotation(mRotation, RotationEncoderOffset.kEncoderFrontRightOffset, kEncoderFrontRightRotation.getAbsolutePosition()), 0));
+        }
+        else {
+          kMotorFrontRightRotation.set(0);
+        }
+        if (Math.abs(kEncoderBackRightRotation.getAbsolutePosition() - getMotorRotation(mRotation, RotationEncoderOffset.kEncoderBackRightOffset, kEncoderBackRightRotation.getAbsolutePosition())) > mError) {
+          kMotorBackRightRotation.set(mTurnSpeed * getSign(kEncoderBackRightRotation.getAbsolutePosition(), getMotorRotation(mRotation, RotationEncoderOffset.kEncoderBackRightOffset, kEncoderBackRightRotation.getAbsolutePosition()), 1));
+        }
+        else {
+          kMotorBackRightRotation.set(0);
+        }
+        if (Math.abs(kEncoderFrontLeftRotation.getAbsolutePosition() - getMotorRotation(mRotation, RotationEncoderOffset.kEncoderFrontLeftOffset, kEncoderFrontLeftRotation.getAbsolutePosition())) > mError) {
+          kMotorFrontLeftRotation.set(mTurnSpeed * getSign(kEncoderBackRightRotation.getAbsolutePosition(), getMotorRotation(mRotation, RotationEncoderOffset.kEncoderBackRightOffset, kEncoderFrontLeftRotation.getAbsolutePosition()), 2));
+        }
+        else {
+          kMotorFrontLeftRotation.set(0);
+        }
+        if (Math.abs(kEncoderBackLeftRotation.getAbsolutePosition() - getMotorRotation(mRotation, RotationEncoderOffset.kEncoderBackLeftOffset, kEncoderBackLeftRotation.getAbsolutePosition())) > mError) {
+          kMotorBackLeftRotation.set(mTurnSpeed * getSign(kEncoderBackRightRotation.getAbsolutePosition(), getMotorRotation(mRotation, RotationEncoderOffset.kEncoderBackRightOffset, kEncoderBackLeftRotation.getAbsolutePosition()), 3));
+        }
+        else {
+          kMotorBackLeftRotation.set(0);
+        }
+      }
+    
+      public double getDistance(double encoder, double controller, int id) {
+        if (controller > encoder) {
+          return (encoder - controller) * ((controller - encoder > 0.5) ? -1 : 1);
+        }
+        if (encoder > controller) {
+          return (encoder - controller) * ((encoder - controller > 0.5) ? -1 : 1);
+        }
+        else {
+          return 0;
+        }
+      }
+    
+      public double getSign(double encoder, double controller, int id) {
+        return Math.signum(getDistance(encoder, controller, id));
+      }
+    
+      public void rotate(double speed) {
+        speed *= -mSlowDown;
+        kMotorFrontRightDrive.set(-speed);
+        kMotorBackRightDrive.set(-speed);
+        kMotorFrontLeftDrive.set(-speed);
+        kMotorBackLeftDrive.set(speed);
+        mRotation = 0.25/2 + 0.25;
+        if (Math.abs(kEncoderFrontRightRotation.getAbsolutePosition() - getMotorRotation(mRotation, RotationEncoderOffset.kEncoderFrontRightOffset, kEncoderFrontRightRotation.getAbsolutePosition())) > mError) {
+          kMotorFrontRightRotation.set(mTurnSpeed * getSign(kEncoderFrontRightRotation.getAbsolutePosition(), getMotorRotation(mRotation, RotationEncoderOffset.kEncoderFrontRightOffset, kEncoderFrontRightRotation.getAbsolutePosition()), 0));
+        }
+        else {
+          kMotorBackRightRotation.set(0);
+        }
+        mRotation = 0.25/2 + 0.50;
+        if (Math.abs(kEncoderBackRightRotation.getAbsolutePosition() - getMotorRotation(mRotation, RotationEncoderOffset.kEncoderBackRightOffset, kEncoderBackRightRotation.getAbsolutePosition())) > mError) {
+          kMotorBackRightRotation.set(mTurnSpeed * getSign(kEncoderBackRightRotation.getAbsolutePosition(), getMotorRotation(mRotation, RotationEncoderOffset.kEncoderBackRightOffset, kEncoderBackRightRotation.getAbsolutePosition()), 1));
+        }
+        else {
+          kMotorBackRightRotation.set(0);
+        }
+        mRotation = 0.25/2;
+        if (Math.abs(kEncoderFrontLeftRotation.getAbsolutePosition() - getMotorRotation(mRotation, RotationEncoderOffset.kEncoderFrontLeftOffset, kEncoderFrontLeftRotation.getAbsolutePosition())) > mError) {
+          kMotorFrontLeftRotation.set(mTurnSpeed * getSign(kEncoderBackRightRotation.getAbsolutePosition(), getMotorRotation(mRotation, RotationEncoderOffset.kEncoderBackRightOffset, kEncoderFrontLeftRotation.getAbsolutePosition()), 1));
+        }
+        else {
+          kMotorFrontLeftRotation.set(0);
+        }
+        mRotation = 0.25/2 + 0.75;
+        if (Math.abs(kEncoderBackLeftRotation.getAbsolutePosition() - getMotorRotation(mRotation, RotationEncoderOffset.kEncoderBackLeftOffset, kEncoderBackLeftRotation.getAbsolutePosition())) > mError) {
+          kMotorBackLeftRotation.set(mTurnSpeed * getSign(kEncoderBackRightRotation.getAbsolutePosition(), getMotorRotation(mRotation, RotationEncoderOffset.kEncoderBackRightOffset, kEncoderBackLeftRotation.getAbsolutePosition()), 1));
+        }
+        else {
+          kMotorBackLeftRotation.set(0);
+        }
+      }
+    
+      private void setDrive(double speed, double rotation) {
+        mSpeed = speed;
+        if (rotation != -1)
+          mRotation = rotation;
+      }
+    
+      private double getMotorRotation(double value, double zero, double encoder) {
+        value += zero;
+        if (value > kMaxValue) {
+          value -= kMaxValue;
+        }
+        return value;
+      }
+
+    @Override
+    public void onEnabledLoopStart(double timestamp) {
+        synchronized (Drive.this) {
+            stop();
+        }
+    }
+
+    @Override
+    public void onEnabledLoop(double timestamp) {
+        synchronized (Drive.this) {
+
+        }
+    }
+
+    @Override
+    public void onEnabledLoopStop(double timestamp) {
+        synchronized (Drive.this) {
+            stop();
+        }
+    }
+
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void outputTelemetry() {
+        SmartDashboard.putNumber("Front Left", kEncoderFrontLeftRotation.getAbsolutePosition());
+        SmartDashboard.putNumber("Back Left", kEncoderBackLeftRotation.getAbsolutePosition());
+        SmartDashboard.putNumber("Front Right", kEncoderFrontRightRotation.getAbsolutePosition());
+        SmartDashboard.putNumber("Back Right", kEncoderBackRightRotation.getAbsolutePosition());
+    }
+
+    @Override
+    public boolean checkSystem() {
+        // TODO Auto-generated method stub
+        return false;
+    }
 }
